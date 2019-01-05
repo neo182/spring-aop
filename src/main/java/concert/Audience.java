4d7526825e3a3d39776f4394865ebae35535d855
neo182 <meet_neo1@gmail.com>
2019-01-06 00:58:28 +0100

    Reimplementing the Audience aspect using @Around

--- src/main/java/concert/Audience.java
+++ src/main/java/concert/Audience.java
@@ -1,35 +1,29 @@
 package concert;
 
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.Pointcut;		
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class Audience {
+	// Declaring a named Pointcut
 	@Pointcut("execution(** concert.Performance.perform(..))")
-	public void performance() {};
-	
-	@Before("performance()")
-	public void silenceCellPhones() {
-		System.out.println("Audiences silencing cell phones.");
-	}
+	public void performance() {
+	};
 
-	@Before("performance()")
-	public void takeSeats() {
-		System.out.println("Audiences taking seats.");
-	}
-
-	@AfterReturning("performance()")
-	public void applause() {
-		System.out.println("Clap Clap Clap!");
-	}
-
-	@AfterThrowing("performance()")
-	public void demandRefund() {
-		System.out.println("Audiences demaning refund.");
+	// Around annotation this time
+	@Around("performance()")
+	public void watchPerformance(ProceedingJoinPoint jp) { // note ProceedingJointPoint here
+		try {
+			System.out.println("Audiences silencing cell phones.");
+			System.out.println("Audiences taking seats.");
+			jp.proceed();
+			System.out.println("Clap Clap Clap!");
+		} catch (Throwable e) {
+			System.out.println("Audiences demanding refund.");
+		}
 	}
 }
