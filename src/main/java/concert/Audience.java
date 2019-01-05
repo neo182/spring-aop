19b2ebad0e6deba96b95bb607c78eeecb7ce8db7
neo182 <meet_neo1@gmail.com>
2019-01-06 00:49:30 +0100

    Using a Pointcut to make things easier

--- src/main/java/concert/Audience.java
+++ src/main/java/concert/Audience.java
@@ -4,27 +4,31 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class Audience {
-	@Before("execution(** concert.Performance.perform(..))")
+	@Pointcut("execution(** concert.Performance.perform(..))")
+	public void performance() {};
+	
+	@Before("performance()")
 	public void silenceCellPhones() {
 		System.out.println("Audiences silencing cell phones.");
 	}
 
-	@Before("execution(** concert.Performance.perform(..))")
+	@Before("performance()")
 	public void takeSeats() {
 		System.out.println("Audiences taking seats.");
 	}
 
-	@AfterReturning("execution(** concert.Performance.perform(..))")
+	@AfterReturning("performance()")
 	public void applause() {
 		System.out.println("Clap Clap Clap!");
 	}
 
-	@AfterThrowing("execution(** concert.Performance.perform(..))")
+	@AfterThrowing("performance()")
 	public void demandRefund() {
 		System.out.println("Audiences demaning refund.");
 	}
